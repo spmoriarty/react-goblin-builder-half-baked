65c9a2566f937e31ad4fac4d92c027d27a4dce70
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-16 17:15:12 -0700

    set states on app.js, working on handlers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import GoblinForm from './GoblinForm';
 import GoblinList from './GoblinList';
 import Goblin from './Goblin';
+import { useState } from 'react';
 
 function App() {
   /* 
@@ -12,12 +13,18 @@ function App() {
       goblinFormHP, which is how we track the user input for the current HP of the goblin in the form
       goblinFormColor, which is how we track the user input for the current color of the goblin in the form
 */
-  
-  function submitGoblin(e) {
-    e.preventDefault()
-    
-    // on submit, make a new goblin object with a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
+const [allGoblins, setAllGoblins] = useState('');
+const [goblinFormName, setGoblinFormName] = useState('');
+const [goblinFormHP, setGoblinFormHP] = useState('');
+const [goblinFormColor, setGoblinFormColor] = useState('');
 
+
+
+  function submitGoblin(e) {
+    e.preventDefault();
+    const [newGoblin, setNewGoblin] = useState('');
+    // on submit, make a new goblin object with a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
+    setAllGoblins([...allGoblins, newGoblin]);
     // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
     
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
